63bd0f191349f051c971c91fbc13a3da79c1db54
Aman <amsingh714@gmail.com>
2019-03-18 18:13:53 -0400

    able to PUT & DELETE user

--- index.js
+++ index.js
@@ -55,19 +55,61 @@ server.post("/api/users", (req, res) => {
         res.status(201).json(result);
       })
       .catch(error => {
-        res
-          .status(500)
-          .json({
-            error: "There was an error while saving the user to the database"
-          });
+        res.status(500).json({
+          error: "There was an error while saving the user to the database"
+        });
       });
   }
 });
 
-
-
 // PUT request to /api/users/:id
+server.put("/api/users/:id", (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+
+  db.findById(id).then(user => {
+    if (!user) {
+      res
+        .status(404)
+        .json({ message: "The user with the specified ID does not exist." });
+    }
+    if (!changes.name || !changes.bio) {
+      res
+        .status(400)
+        .json({ errorMessage: "Please provide name and bio for the user." });
+    }
+
+    db.update(id, changes)
+      .then(result => {
+        res.status(200).json({ result });
+      })
+      .catch(error => {
+        res.status(500).json({
+          error: "The user information could not be modified."
+        });
+      });
+  });
+});
 
 // DELETE request to /api/users/:id
+server.delete("/api/users/:id", (req, res) => {
+  const { id } = req.params;
+
+  db.findById(id)
+  .then(user => {
+    if (user) {
+      db.remove(id).then(deleted => {
+        res.status(200).json(user)
+      })
+    } else {
+      res.status(400).json({ message: "The user with the specified ID does not exist." })
+    }
+  })
+  .catch(error => {
+    res.status(500).json({
+      error: "The user could not be removed"
+    })
+  })
+})
 
 server.listen(port, () => console.log(`Port: ${port}`));
